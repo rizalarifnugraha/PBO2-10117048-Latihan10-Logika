e1458d6cc83f8613153b51f6f62e8211a1838dde
rizalarifnugraha <43487664+rizalarifnugraha@users.noreply.github.com>
2018-09-22 19:55:28 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117048/latihan10/logika/PBO210117048Latihan10Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * Nama  : Rizal Arif Nugraha
+ * Kelas : PBO2 
+ * NIM   : 10117048
+ * Deskripsi Program : Program ini berisi program menggunakan logika dengan
+ * boolean.
+
+ */
+public class PBO210117048Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
